0bc7ab2703b8fb7d4f89878457ec8b82b604fea3
NDKYLJK <65161313+NDKYLJK@users.noreply.github.com>
2020-06-02 14:49:46 +0800

    Add files via upload

---
+++ LOGINandREG/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//要在视图里views中也建立一个Login文件夹
+namespace LOGINandREG.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
